80d6fd2a9d2b0bc29d7798438e9cd99991f32849
jdkohls@ucsc.edu <jdkohls@ucsc.edu>
2023-05-03 12:04:36 -0400

    fuzzer update to not just return emptystring

--- fuzz/fuzz.go
+++ fuzz/fuzz.go
@@ -1,10 +1,17 @@
 package fuzz
 
 import(
-	tea "github.com/Pauloo27/tuner/search"
+	search "github.com/Pauloo27/tuner/search"
 )
 
 func Fuzz(fuzz_bytes []byte) int{
-	tea.Search(string(fuzz_bytes),10	)
+	sources := []search.SearchSource{search.SourceYouTube}
+	sources = append(sources, &search.SourceSoundCloud)
+	result := search.Search(string(fuzz_bytes), 10, sources...)
+	if result == nil{
+		panic("Result is nil")
+	}
+
 	return 0
+
 }
