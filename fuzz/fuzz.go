88d1710afe2e5d398b5e2a63622baac7b5fe8a3c
jdkohls@ucsc.edu <jdkohls@ucsc.edu>
2023-05-03 22:42:52 -0400

    changed youtube search to sounfdcloud

--- fuzz/fuzz.go
+++ fuzz/fuzz.go
@@ -2,13 +2,14 @@ package fuzz
 
 import(
 	search "github.com/Pauloo27/tuner/search"
-	"fmt"
+	_ "fmt"
 )
 
 func Fuzz(fuzz_bytes []byte) int{
-	sources := []search.SearchSource{search.SourceYouTube}
+	//sources := []search.SearchSource{search.SourceYouTube}
+	sources := []search.SearchSource{search.SourceSoundCloud}
 	_ = search.Search(string(fuzz_bytes), 10, sources...)
-	fmt.Println("done")
+	//fmt.Println("done")
 
 	return 0
 
