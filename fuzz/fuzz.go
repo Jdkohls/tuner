50a21db0b4e3dd573541d4a2b29d37f1c3717ac5
jdkohls@ucsc.edu <jdkohls@ucsc.edu>
2023-05-03 22:31:22 -0400

    removed soundcloud search

--- fuzz/fuzz.go
+++ fuzz/fuzz.go
@@ -2,15 +2,13 @@ package fuzz
 
 import(
 	search "github.com/Pauloo27/tuner/search"
+	"fmt"
 )
 
 func Fuzz(fuzz_bytes []byte) int{
 	sources := []search.SearchSource{search.SourceYouTube}
-	sources = append(sources, &search.SourceSoundCloud)
-	result := search.Search(string(fuzz_bytes), 10, sources...)
-	if result == nil{
-		panic("Result is nil")
-	}
+	_ = search.Search(string(fuzz_bytes), 10, sources...)
+	fmt.Println("done")
 
 	return 0
 
